0aaaa1f6f2697ad0d7e46b5cece8156c3c9ae08c
Don1k1337 <dan4ik1411@gmail.com>
2022-02-07 16:45:49 +0600

    Files added

---
+++ inventory/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    path('', views.StockListView.as_view(), name='inventory'),
+    path('new', views.StockCreateView.as_view(), name='new-stock'),
+    path('stock/<pk>/edit', views.StockUpdateView.as_view(), name='edit-stock'),
+    path('stock/<pk>/delete', views.StockDeleteView.as_view(), name='delete-stock'),
+    path('inventory/export-excel', views.export_excel_av_items, name='export-excel'),
+]
---
+++ transactions/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import (
+    Supplier, 
+    PurchaseBill, 
+    PurchaseItem,
+    PurchaseBillDetails
+)
+
+admin.site.register(Supplier)
+admin.site.register(PurchaseBill)
+admin.site.register(PurchaseItem)
+admin.site.register(PurchaseBillDetails)
